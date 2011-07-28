9e2e6bcf08e6a41d3311e901511e99bef2e2f9d9
suVene <suvene@zeromemory.info>
2011-07-28 11:19:26 +0900

    initial import

---
+++ G+.mine.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name G+.mine.user.js
+// @author suVene
+// @version 0.1.0
+// @namespace https://github.com/suvene/G+.mine.user.js
+// @description GooglePlus custmize for mine
+// @include https://plus.google.com/*
+// @match https://plus.google.com/*
+// ==/UserScript==
+
+(function() {
+  // User Options start
+
+  // User Options end
+
+
+})()
