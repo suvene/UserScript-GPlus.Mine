33252efaecca173cb18bcf79eae468a00f744ce4
suVene <suvene@zeromemory.info>
2011-07-28 11:38:07 +0900

    add autopagerize

--- G+.mine.user.js
+++ G+.mine.user.js
@@ -10,8 +10,40 @@
 
 (function() {
   // User Options start
+  var AutoPagerize = true; // true or false
 
   // User Options end
 
+  function invokeMouseEvent(elem, evname) {
+    var e = document.createEvent('MouseEvents');
+    e.initMouseEvent(evname, true, true, e.view||window, 0, 0, 0, 0, 0,
+      false, false, false, false, 0, null);
+    elem.dispatchEvent(e);
+  }
+
+  function click(elem) {
+    invokeMouseEvent(elem, 'click');
+  }
+
+  function mousedown(elem) {
+    invokeMouseEvent(elem, 'mousedown');
+  }
+
+  function mouseup(elem) {
+    invokeMouseEvent(elem, 'mouseup');
+  }
+
+  function installScrollListener() {
+    window.addEventListener('scroll', function() {
+      if (AutoPagerize) {
+        var b = parseInt((window.scrollY + window.innerHeight) / document.height + 0.1);
+        if (b) {
+          click(document.getElementsByClassName('a-f-zd-gb-h')[0]);
+        }
+      }
+    }, false);
+  }
+
+  installScrollListener();
 
 })()
